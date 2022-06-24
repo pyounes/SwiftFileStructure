da7b00c40d13dc4a06c7c3f7ea8d2a05256e11f4
Pierre Y <pierre.youness@montyholding.com>
2022-06-24 11:04:34 +0300

    generating uitableviewcell files

---
+++ MVVM Module.xctemplate/UITableViewCellXIBSwift/___FILEBASENAME___TableViewCell.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_subclass___ {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+
+    // MARK: - Configure cell from View Model
+    func configure(with viewModel: ___VARIABLE_productName:identifier___TableViewCellVM) {
+        
+    }
+}
