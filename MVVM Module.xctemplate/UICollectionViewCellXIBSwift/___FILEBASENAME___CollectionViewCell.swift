1430be33d64fa5168c27050a2d151abba39b887b
Pierre Y <pierre.youness@montyholding.com>
2022-06-24 12:36:42 +0300

    added support for uicollectionview

---
+++ MVVM Module.xctemplate/UICollectionViewCellXIBSwift/___FILEBASENAME___CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_subclass___ {
+
+    // MARK: - Configure cell from View Model
+    func configure(with viewModel: ___VARIABLE_productName:identifier___CollectionViewCellVM) {
+        
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: CellIdentifiable  { }
